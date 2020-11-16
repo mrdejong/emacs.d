9115db3af8443000aeeab953597b4336947a8cf9
Alexander de Jong <mrdejong89@gmail.com>
2020-11-16 11:11:22 +0100

    Add binding for magit status
    * This will make it easier to get to the status page.

--- init.el
+++ init.el
@@ -193,13 +193,17 @@
 (use-package evil-magit
   :after magit)
 
+(dc/leader-keys
+  "vs" '(magit-status :which-key "Version control"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil-magit magit counsel-projectile projectile hydra evil-collection evil general doom-themes helpful counsel ivy-rich which-key rainbow-delimiters doom-modeline diminish ivy use-package)))
+   (quote
+    (evil-magit magit counsel-projectile projectile hydra evil-collection evil general doom-themes helpful counsel ivy-rich which-key rainbow-delimiters doom-modeline diminish ivy use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
